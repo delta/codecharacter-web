9e29d5f7b9c6782c11da6c91e643a27dbab4f4ae
Ram <ram2091999@users.noreply.github.com>
2020-01-18 13:13:32 +0530

    Add PopUp Menu

---
+++ src/app/components/PopUpMenu.tsx
@@ -0,0 +1,63 @@
+/* tslint:disable-next-line:import-name */
+import CastConnectedIcon from '@material-ui/icons/CastConnected';
+/* tslint:disable-next-line:import-name */
+import ClearIcon from '@material-ui/icons/Clear';
+/* tslint:disable-next-line:import-name */
+import DescriptionIcon from '@material-ui/icons/Description';
+/* tslint:disable-next-line:import-name */
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+/* tslint:disable-next-line:import-name */
+import GolfCourseIcon from '@material-ui/icons/GolfCourse';
+import * as styles from 'app/styles/PopUpMenu.module.css';
+import classnames from 'classnames';
+import * as React from 'react';
+
+export default class PopUpMenu extends React.Component<{}, { isPopUpOpen: boolean }> {
+  /* tslint:disable-next-line */
+  public constructor(props: any) {
+    super(props);
+    this.state = {
+      isPopUpOpen: false,
+    };
+    this.togglePopUp = this.togglePopUp.bind(this);
+    this.setPopUpToFalse = this.setPopUpToFalse.bind(this);
+  }
+  public togglePopUp() {
+    this.setState({ isPopUpOpen: !this.state.isPopUpOpen });
+  }
+  public setPopUpToFalse() {
+    this.setState({ isPopUpOpen: false });
+  }
+
+  public render() {
+    return (
+      <div>
+        <div
+          className={
+            !this.state.isPopUpOpen
+              ? classnames(styles['pop-up-display-none'])
+              : classnames(styles['pop-up'])
+          }
+        >
+          <ClearIcon className={classnames(styles['close-icon'])} onClick={this.togglePopUp} />
+          <div className={classnames(styles['menu-item'], styles['menu-item-github'])}>
+            <GitHubIcon className={classnames(styles['menu-icon'])} />
+            Github
+          </div>
+          <div className={classnames(styles['menu-item'], styles['menu-item-documentation'])}>
+            <DescriptionIcon className={classnames(styles['menu-icon'])} />
+            Documentation
+          </div>
+          <div className={classnames(styles['menu-item'], styles['menu-item-slack'])}>
+            <CastConnectedIcon className={classnames(styles['menu-icon'])} />
+            Slack
+          </div>
+        </div>
+        <button className={classnames(styles['main-button'])} onClick={this.togglePopUp}>
+          <GolfCourseIcon />
+        </button>
+      </div>
+    );
+  }
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
+import PopUpMenu from 'app/components/PopUpMenu';
 import Login from 'app/containers/Authentication/Login';
 import Register from 'app/containers/Authentication/Register';
 import Dashboard from 'app/containers/Dashboard';
 import LandingPage from 'app/containers/LandingPage';
+
 // @ts-ignore
 import { initializeRendererAssets } from 'codecharacter-renderer';
 import * as React from 'react';
@@ -18,11 +20,12 @@ export const App = hot(module)(() => (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        <Route exact path="/landing" component={LandingPage} />
+        <Route exact path="/home" component={LandingPage} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
+    <PopUpMenu />
     <Sugar background="#484848" color="white" />
   </React.Fragment>
 ));
