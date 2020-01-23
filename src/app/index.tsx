1186136525f70c9f7940de14824389eda0eea0b1
Ram <ram2091999@users.noreply.github.com>
2020-01-23 21:31:05 +0530

    Remove landingPage files

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,20 @@
 import Dashboard from 'app/containers/Dashboard';
-import LandingPage from 'app/containers/LandingPage';
 // @ts-ignore
 import { initializeRendererAssets } from 'codecharacter-renderer';
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
 // @ts-ignore
 import { Sugar } from 'react-preloaders';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 
 initializeRendererAssets();
 
 /* tslint:disable-next-line:variable-name */
 export const App = hot(module)(() => (
   <React.Fragment>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/home" component={LandingPage} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" component={Dashboard} />
+    </Switch>
     <Sugar background="#484848" color="white" />
   </React.Fragment>
 ));
