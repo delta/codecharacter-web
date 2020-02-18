78e1427f8e929dc3055a2a0f951fa09211c66e32
Ram <ram2091999@users.noreply.github.com>
2020-02-18 18:27:00 +0530

    Fix linter errors

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -6,46 +6,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import classnames from 'classnames';
 import * as React from 'react';
 
-
-export class LandingPage extends React.Component<
-  LandingPageInterfaces.Props,
-  {}
-> {
+export class LandingPage extends React.Component<LandingPageInterfaces.Props, {}> {
   public constructor(props: LandingPageInterfaces.Props) {
     super(props);
   }
 
-
-
-
   public render() {
     return (
       <div className={classnames(styles.root)}>
+        <div className={classnames(styles.nav)}>
+          <input type="checkbox" className={classnames(styles['nav-check'])} id="nav-check" />
+          <div className={classnames(styles['nav-header'])}>
+            <div className={classnames(styles['nav-title'])}>JoGeek</div>
+          </div>
+          <div className={classnames(styles['nav-btn'])}>
+            <label htmlFor="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
 
-      <div className={classnames(styles.nav)}>
- <input type="checkbox" className={classnames(styles["nav-check"])} id="nav-check"/>
- <div className={classnames(styles["nav-header"])}>
-   <div className={classnames(styles["nav-title"])}>
-     JoGeek
-   </div>
- </div>
- <div className={classnames(styles["nav-btn"])}>
-   <label htmlFor="nav-check">
-     <span></span>
-     <span></span>
-     <span></span>
-   </label>
- </div>
-
- <div className={classnames(styles["nav-links"])}>
-   <a href="//github.io/jo_geek" target="_blank">Github</a>
-   <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
-   <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
-   <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
-   <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
- </div>
-</div>
-
+          <div className={classnames(styles['nav-links'])}>
+            <a href="//github.io/jo_geek" target="_blank">
+              Github
+            </a>
+            <a href="http://stackoverflow.com/users/4084003/" target="_blank">
+              Stackoverflow
+            </a>
+            <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">
+              LinkedIn
+            </a>
+            <a href="https://codepen.io/jo_Geek/" target="_blank">
+              Codepen
+            </a>
+            <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">
+              JsFiddle
+            </a>
+          </div>
+        </div>
 
         <div className={classnames('container', styles['first-cont'])}>
           <div className="row">
@@ -59,7 +58,7 @@ export class LandingPage extends React.Component<
                   return (
                     <FaqElement key={index} question={element.question} answer={element.answer} />
                   );
-                }else{
+                } else {
                   return null;
                 }
               })}
@@ -70,7 +69,7 @@ export class LandingPage extends React.Component<
                   return (
                     <FaqElement key={index} question={element.question} answer={element.answer} />
                   );
-                }else{
+                } else {
                   return null;
                 }
               })}
