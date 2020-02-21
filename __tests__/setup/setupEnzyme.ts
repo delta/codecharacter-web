dafe687a8dd2a1dad07e2b9896d58e49164d18e1
Chethan2k1 <40890937+Chethan2k1@users.noreply.github.com>
2020-02-21 16:28:59 +0530

    Fix fetch bug

--- __tests__/setup/setupEnzyme.ts
+++ __tests__/setup/setupEnzyme.ts
@@ -1,3 +1,13 @@
+// tslint:disable-next-line
+const { Response, Request, Headers, fetch } = require('whatwg-fetch');
+// tslint:disable-next-line
+var global: any;
+
+global.Response = Response;
+global.Request = Request;
+global.Headers = Headers;
+global.fetch = fetch;
+
 import { configure } from 'enzyme';
 import * as EnzymeAdapter from 'enzyme-adapter-react-16';
 configure({ adapter: new EnzymeAdapter() });
