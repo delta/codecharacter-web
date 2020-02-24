e257a808d049df6bba72473206912fa17f6a3e65
Chethan2k1 <40890937+Chethan2k1@users.noreply.github.com>
2020-02-24 20:33:29 +0530

    Resovle 'fetch' in nodejs enivronment using whatwg-fetch polyfill (#27)
    - Remove redundant type declarations
    - Add esModuleinterop in tsconfig.json

--- __tests__/setup/setupEnzyme.ts
+++ __tests__/setup/setupEnzyme.ts
@@ -1,3 +1,21 @@
+// tslint:disable-next-line
+const { Response, Request, Headers, fetch } = require('whatwg-fetch');
+
+// @ts-ignore
+const global = {
+  Headers,
+  Request,
+  Response,
+  fetch,
+};
+
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// Enzyme.configure({ adapter: new Adapter() })
+
 import { configure } from 'enzyme';
-import * as EnzymeAdapter from 'enzyme-adapter-react-16';
-configure({ adapter: new EnzymeAdapter() });
+import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
+// tslint:disable:no-any
+const adapter = ReactSixteenAdapter as any;
+configure({ adapter: new adapter.default() });
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,8 @@
-/** Global definitions for development **/
+/* Global definitions for development */
 
 // for style loader
 declare module '*.css' {
+  // tslint:disable-next-line:no-any
   const styles: any;
   export = styles;
 }
-
-// Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-type PartialPick<T, K extends keyof T> = Partial<T> & Pick<T, K>;
