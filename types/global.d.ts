41cbfe578f664b8590dcd27a467f7fcad6858e60
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-02-24 20:01:58 +0530

    Remove redundant type declarations

--- types/global.d.ts
+++ types/global.d.ts
@@ -6,6 +6,3 @@ declare module '*.css' {
   const styles: any;
   export = styles;
 }
-
-// Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
-type PartialPick<T, K extends keyof T> = Partial<T> & Pick<T, K>;
