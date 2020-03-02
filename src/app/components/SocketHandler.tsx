932d8dcc73e9fe3337904169630ae22d2276ad16
Sai Hemanth <saihemanth9019@gmail.com>
2020-03-02 14:09:10 +0530

    Integrate sockjs

--- src/app/components/SocketHandler.tsx
+++ src/app/components/SocketHandler.tsx
@@ -1,109 +1,44 @@
-import { SOCKET_BASE_URL } from 'app/../config/config';
 import * as SocketHandlerInterfaces from 'app/types/SocketHandler';
 import * as React from 'react';
-import * as io from 'socket.io-client';
+
+// tslint:disable-next-line:import-name
+import { Stomp } from '@stomp/stompjs';
+// tslint:disable-next-line:import-name
+import SockJsClient from 'sockjs-client';
+import { SOCKET_BASE_URL } from '../../config/config';
 
 export class SocketHandler extends React.Component<SocketHandlerInterfaces.Props, {}> {
-  private socket: SocketIOClient.Socket;
+  private readonly socket: WebSocket;
+  private stompClient : Stomp;
+
   constructor(props: SocketHandlerInterfaces.Props) {
     super(props);
-    this.socket = io.connect(SOCKET_BASE_URL, {
-      reconnection: true,
-      reconnectionDelay: 1000,
-      transports: ['websocket'],
+    this.socket = new SockJsClient(`${SOCKET_BASE_URL}`);
+    this.stompClient = Stomp.over(this.socket);
+    // @ts-ignore
+    this.stompClient.connect({}, (frame) => {
+      // TODO: Change to user's actual id
+      const userId = 4;
+      // @ts-ignore
+      this.stompClient.subscribe(`/response/${userId}`, (message: { body: string; }) => {
+        // tslint:disable-next-line:no-console
+        console.log(`Received message: ${message.body}`);
+      });
     });
   }
 
-  public componentDidMount() {
-    const {
-      sendCompileError,
-      sendCompileSuccess,
-      sendExecuteError,
-      sendExecuteSuccess,
-      sendInfo,
-      sendSuccess,
-      sendError,
-      sendDebugRunSuccess,
-      sendDebugRunError,
-    } = this.props;
-
-    this.socket.on('Info', (message: string) => {
-      sendInfo(message);
-    });
-
-    this.socket.on('Success', (message: string) => {
-      sendSuccess(message);
-    });
-
-    this.socket.on('Error', (message: string) => {
-      sendError(message);
-    });
-
-    this.socket.on('connect', () => {
-      sendSuccess('Connected to Server!');
-    });
-
-    this.socket.on('Compile Info', (message: string) => {
-      sendInfo(message);
-    });
-
-    this.socket.on('Compile Success', () => {
-      sendSuccess('Compiled Successfully!');
-      sendCompileSuccess();
-    });
-
-    this.socket.on('Compile Error', (message: string) => {
-      sendError(`Compile Error: ${message}`);
-      sendCompileError('');
-    });
-
-    this.socket.on('Compile Error Log', (log: string) => {
-      sendError('Compile Error');
-      sendCompileError(log);
-    });
-
-    this.socket.on('Match Info', (message: string) => {
-      sendInfo(message);
-    });
-
-    this.socket.on('Match Error', (message: string) => {
-      sendError(message);
-      sendExecuteError(message);
-    });
-
-    this.socket.on('Match Result Success', (result: string) => {
-      sendSuccess(result);
-    });
-
-    this.socket.on('Match Result Error', (result: string) => {
-      sendError(result);
-    });
-
-    this.socket.on('Match Success', (matchLogs: string) => {
-      sendSuccess('Match Executed Successfully!');
-      sendExecuteSuccess(matchLogs);
-    });
-
-    this.socket.on('Debug Run Info', (message: string) => {
-      sendInfo(message);
-    });
-
-    this.socket.on('Debug Run Success', (stackTrace: string) => {
-      sendDebugRunSuccess(stackTrace);
-    });
-
-    this.socket.on('Debug Run Error', (message: string) => {
-      sendError(`Debug Run Error: ${message}`);
-      sendDebugRunError();
-    });
-
-    this.socket.on('disconnect', () => {
-      sendError('Disconnected');
-    });
+  public initiateMatch(playerId1: number, playerId2: number, matchMode: string, mapId: number): void {
+    // @ts-ignore
+    this.stompClient.send('/request/match', {}, JSON.stringify({
+      'matchMode': matchMode,
+      'playerId1': playerId1,
+      'playerId2': playerId2
+    }));
   }
 
-  public componentWillUnmount() {
-    this.socket.disconnect();
+  public componentWillUnmount(): void {
+    // @ts-ignore
+    this.stompClient.disconnect();
   }
 
   public render() {
