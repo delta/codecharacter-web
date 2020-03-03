0f546cbda60167774f56d6ace8857db296fbcd76
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-03-03 06:11:40 +0530

    Integrate sockets with hardcoded mapId

--- src/app/containers/SocketHandler.ts
+++ src/app/containers/SocketHandler.ts
@@ -1,12 +1,19 @@
 import { NotificationActions, SubmissionActions } from 'app/actions';
 import { SocketHandler } from 'app/components/SocketHandler';
 import { RootState } from 'app/reducers';
+import * as SubmissionInterfaces from 'app/types/code/Submission';
 import * as SocketHandlerInterfaces from 'app/types/SocketHandler';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 
 const mapStateToProps = (rootState: RootState) => {
-  return {};
+  return {
+    commitHash: rootState.submission.commitHash,
+    mapId: rootState.submission.mapId,
+    playerId1: rootState.submission.playerId1,
+    playerId2: rootState.submission.playerId2,
+    request: rootState.submission.request,
+  };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
@@ -21,10 +28,16 @@ const mapDispatchToProps = (dispatch: Dispatch) => {
     sendExecuteSuccess: (logs: string) => dispatch(SubmissionActions.handleExecuteSuccess(logs)),
     sendInfo: (message: string) => dispatch(NotificationActions.info(message)),
     sendSuccess: (message: string) => dispatch(NotificationActions.success(message)),
+    updateRequest: (request: SubmissionInterfaces.Request) =>
+      dispatch(SubmissionActions.changeCurrentRequest(request)),
   };
 };
 
-const socketHandlerContainer = connect<{}, SocketHandlerInterfaces.DispatchProps, {}>(
+const socketHandlerContainer = connect<
+  SocketHandlerInterfaces.StateProps,
+  SocketHandlerInterfaces.DispatchProps,
+  {}
+>(
   mapStateToProps,
   mapDispatchToProps,
 )(SocketHandler);
