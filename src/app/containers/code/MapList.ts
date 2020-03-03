1645cbfa9c483929802198acc3270e789b401fd0
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-03-03 06:11:40 +0530

    Integrate sockets with hardcoded mapId

---
+++ src/app/containers/code/MapList.ts
@@ -0,0 +1,34 @@
+import { SubmissionActions } from 'app/actions';
+import { MapList } from 'app/components/code/CommitLog/MapList';
+import { RootState } from 'app/reducers';
+import { Map, Request } from 'app/types/code/Submission';
+import { connect } from 'react-redux';
+
+export interface State {
+  imgType: string;
+  isHovered: boolean;
+  isMapListOpen: boolean;
+}
+
+export interface DispatchProps {
+  updateRequest: (request: Request) => void;
+  updateMapId: (mapId: number) => void;
+}
+
+export interface MapListProps {
+  maps: Map[];
+  startMatch: (mapId: number) => void;
+}
+
+export type Props = MapListProps & DispatchProps;
+
+const mapStateToProps = (rootState: RootState) => {
+  return {};
+};
+
+const mapListContainer = connect<{}, DispatchProps, {}>(mapStateToProps, {
+  updateMapId: SubmissionActions.updateMapId,
+  updateRequest: SubmissionActions.changeCurrentRequest,
+})(MapList);
+
+export default mapListContainer;
