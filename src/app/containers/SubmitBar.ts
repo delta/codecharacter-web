33f96d1b92c2dd17103561af6c845ae9f82fdca1
Ram <ram2091999@users.noreply.github.com>
2020-03-03 21:32:44 +0530

    Hardcode AiIds

--- src/app/containers/SubmitBar.ts
+++ src/app/containers/SubmitBar.ts
@@ -1,7 +1,7 @@
 import { CodeActions, GameLogActions, SubmissionActions } from 'app/actions';
 import { SubmitBar } from 'app/components/SubmitBar';
 import { RootState } from 'app/reducers';
-import { Request } from 'app/types/code/Submission';
+import * as SubmissionInterfaces from 'app/types/code/Submission';
 import * as SubmitBarInterfaces from 'app/types/SubmitBar';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
@@ -9,7 +9,7 @@ import { Dispatch } from 'redux';
 const mapStateToProps = (rootState: RootState) => {
   return {
     aiIds: rootState.submission.aiIds,
-    debugRunAvailable: rootState.submission.debugRunRequest !== Request.NONE,
+    debugRunAvailable: rootState.submission.debugRunRequest !== SubmissionInterfaces.Request.NONE,
     maps: rootState.submission.maps,
   };
 };
@@ -17,6 +17,8 @@ const mapStateToProps = (rootState: RootState) => {
 const mapDispatchToProps = (dispatch: Dispatch) => {
   return {
     aiMatch: (mapId: number, aiId: number) => dispatch(SubmissionActions.aiMatch(mapId, aiId)),
+    changeCurrentRequest: (currentRequest: SubmissionInterfaces.Request) =>
+      dispatch(SubmissionActions.changeCurrentRequest(currentRequest)),
     clearLogs: () => {
       dispatch(GameLogActions.updateGameLog('', '', ''));
       dispatch(GameLogActions.clearDisplayDebugLog());
@@ -29,6 +31,7 @@ const mapDispatchToProps = (dispatch: Dispatch) => {
     lockCode: () => dispatch(SubmissionActions.lockCode()),
     saveCode: () => dispatch(CodeActions.save()),
     selfMatch: (mapId: number) => dispatch(SubmissionActions.selfMatch(mapId)),
+    updateMapId: (mapId: number) => dispatch(SubmissionActions.updateMapId(mapId)),
   };
 };
 
