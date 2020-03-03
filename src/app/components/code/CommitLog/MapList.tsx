7399a4ae56df90feec5292d33404ed6ae434b5a7
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-03-03 06:11:40 +0530

    Integrate sockets with hardcoded mapId

--- src/app/components/code/CommitLog/MapList.tsx
+++ src/app/components/code/CommitLog/MapList.tsx
@@ -1,19 +1,39 @@
+import { Props } from 'app/containers/code/MapList';
 import * as styles from 'app/styles/CommitLog.module.css';
-import { MapListProps } from 'app/types/code/CommitElement';
+import { Map, Request } from 'app/types/code/Submission';
 import classnames from 'classnames';
 import * as React from 'react';
 
-export class MapList extends React.Component<MapListProps, {}> {
+export class MapList extends React.Component<Props, {}> {
   public render() {
-    const { maps, startMatch } = this.props;
-
+    const { updateMapId, updateRequest } = this.props;
+    const maps: Map[] = [
+      {
+        mapId: 1,
+        name: 'Map of kings',
+      },
+      {
+        mapId: 2,
+        name: 'The forbidden isle of Zeus',
+      },
+      {
+        mapId: 3,
+        name: 'The End of fucking world',
+      },
+      {
+        mapId: 4,
+        name: 'Not your map',
+      },
+      { mapId: 5, name: 'You cannot win in this one' },
+    ];
     return (
       <div className={classnames(styles.dropdown)}>
         {maps.map((map, index) => (
           <div
             className={classnames(styles.dropdownItem)}
             onClick={(e) => {
-              startMatch(map.mapId);
+              updateRequest(Request.PREVIOUS_COMMIT_MATCH);
+              updateMapId(map.mapId);
               e.stopPropagation();
             }}
           >
