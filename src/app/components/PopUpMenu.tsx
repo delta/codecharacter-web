4636c95b049efdd5e7b219706e8b32edc02c5080
Bestin <bestinbthomas@gmail.com>
2020-03-04 23:02:54 +0530

    fix popup links

--- src/app/components/PopUpMenu.tsx
+++ src/app/components/PopUpMenu.tsx
@@ -37,8 +37,16 @@ export default class PopUpMenu extends React.Component<{}, ElementOwnProps> {
           <div onClick={this.togglePopUp}>
             <FontAwesomeIcon icon={faTimes} className={classnames(styles['close-icon'])} />
           </div>
-          <a href="https://www.pragyan.org/20/" target="_blank">
+          <a href="https://code.pragyan.org/docs" target="_blank">
             <div className={classnames(styles['menu-item'], styles['menu-item-pragyan'])}>
+              <div className={classnames(styles['menu-icon-documentation'])}>
+                <FontAwesomeIcon icon={faBook} />
+              </div>
+              Documentation
+            </div>
+          </a>
+          <a href="https://www.pragyan.org/20/" target="_blank">
+            <div className={classnames(styles['menu-item'], styles['menu-item-documentation'])}>
               <div>
                 <img
                   src="https://www.pragyan.org/20/cms/templates/common/site/images/favicon.ico"
@@ -51,13 +59,10 @@ export default class PopUpMenu extends React.Component<{}, ElementOwnProps> {
             </div>
           </a>
 
-          <div className={classnames(styles['menu-item'], styles['menu-item-documentation'])}>
-            <div className={classnames(styles['menu-icon-documentation'])}>
-              <FontAwesomeIcon icon={faBook} />
-            </div>
-            Documentation
-          </div>
-          <a href="https://www.discourse.org/" target="_blank">
+          <a
+            href="https://forum.pragyan.org/t/code-character-frequently-asked-questions/20"
+            target="_blank"
+          >
             <div className={classnames(styles['menu-item'], styles['menu-item-discourse'])}>
               <div className={classnames(styles['menu-icon-discourse'])}>
                 <img
@@ -66,7 +71,7 @@ export default class PopUpMenu extends React.Component<{}, ElementOwnProps> {
                   src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAABuUlEQVRYhe3XO2sVQRgG4EdPIoIGtRaCIDZGvIGVjUqCYOFfsFW8QfQX2FpoYSeC/8BGROUQxMo6URTFS6HYK3hlPRZ7Fg7rt5ud3cU0eeFrlnkvszM7+w3rWGNsSBy/G6dxErPYiSl8wkcMcQ8ve8wIDuI+Rg3rCY70YTyFG8gSzIv6g1uYbmu+A0stjMs1xPZU8+kxsat5UU+xKSXA7RqxZVzGHLaMa278bKWGd7Op+VH5+pUFfuAcNtZwBziPnwE/w6EmAZ5VmB9vOgOcqAjxcDXigYA0wtkE8wIXKrT21JGuBYRl9a+9CgM8D/SuTg4qCx8LhO7I90QqsjG3jIU60jv/Jt7bwrzAvkDvVR3he0CY6RBgJtD7MjmgvASbA5HfHQJEP7tRXYAIuzoEmA2efU4NcKpDgGjDfagjRCfgW+0/wxeB3mJqgG/ivbEaLgVaI3lTkxTgQQvzefxqoxUFuJhgPJDPPDLPsL9NgHmcwV28x2tcwWF507JVfuAsite8qOtNZhAF6KMey9u7NQnwCNuamNOu+ayqTN7QNpp53wGGGnY/Sekq8FV+KZm8mLzpQXcd/wd/AXbcN82U8XnbAAAAAElFTkSuQmCC"
                 />
               </div>
-              Discourse
+              Forum
             </div>
           </a>
         </div>
