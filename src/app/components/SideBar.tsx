6af7b872073c6d5e033284203af5fae05fbe50aa
tharun571 <atharun05@gmail.com>
2021-01-19 20:36:27 +0530

    Update BootstrapTooltip to Tooltip, Remove unused function and imports

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -21,25 +21,8 @@ import * as SideBarInterfaces from 'app/types/SideBar';
 import classnames from 'classnames';
 import * as React from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
-import Tooltip from '@material-ui/core/Tooltip'
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStylesBootstrap = makeStyles((theme) => ({
-  arrow: {
-    color: theme.palette.common.black,
-  },
-  tooltip: {
-    backgroundColor: theme.palette.common.black,
-    fontSize: 12,
-  },
-  
-}));
-
-function BootstrapTooltip(props: any) {
-  const classes = useStylesBootstrap();
-
-  return <Tooltip arrow classes={classes} {...props} />;
-}
+import Tooltip from '@material-ui/core/Tooltip';
 
 export class Sidebar extends React.Component<SideBarInterfaces.Props, {}> {
   public render() {
@@ -58,171 +41,168 @@ export class Sidebar extends React.Component<SideBarInterfaces.Props, {}> {
           vertical
           className={classnames('w-100 justify-content-center align-items-center', styles.Sidebar)}
         >
-          <BootstrapTooltip title = "Code Character" placement = "right">
-          <Button
-            className={classnames(
-              'py-2 px-auto text-white d-flex justify-content-center',
-              styles.customBtn,
-            )}
-            onClick={() => closeSidePanelTab()}
-          >
-            <FontAwesomeIcon icon={faCode} />
-          </Button>
-          </BootstrapTooltip>
-
-          <BootstrapTooltip title = "Docs" placement = "right">
-          <Button
-            className={classnames('py-2 px-auto documentation-btn-ctrl', styles.customBtn, {
-              [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.DOCS,
-            })}
-            id="docs_button"
-            onClick={() =>
-              sidePanelTab !== SidePanelTab.DOCS
-                ? openSidePanelTab(SidePanelTab.DOCS)
-                : closeSidePanelTab()
-            }
-          >
-            <FontAwesomeIcon icon={faBook} />
-          </Button>
-          </BootstrapTooltip>
-          <BootstrapTooltip title = "Editor Settings" placement = "right">
-          <Button
-            className={classnames('py-2 px-auto editor-settings-btn-ctrl', styles.customBtn, {
-              [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.EDITOR_SETTINGS,
-            })}
-            id="editor_settings_button"
-            onClick={() =>
-              sidePanelTab !== SidePanelTab.EDITOR_SETTINGS
-                ? openSidePanelTab(SidePanelTab.EDITOR_SETTINGS)
-                : closeSidePanelTab()
-            }
-          >
-            <FontAwesomeIcon icon={faCog} />
-          </Button>
-          </BootstrapTooltip>
-          <BootstrapTooltip title = "Leaderboard" placement = "right">
-          <a
-            href="/#/leaderboard"
-            className={classnames('py-2 px-auto leaderboard-btn-ctrl', styles.customBtn, {
-              [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.LEADERBOARD,
-            })}
-            id="leaderboard_button"
-            onClick={() => clearAllLogs()}
-          >
-            <FontAwesomeIcon icon={faTrophy} />
-          </a>
-          </BootstrapTooltip>
-          {isLoggedIn ? (
-            <BootstrapTooltip title = "Commit Log" placement = "right">
+          <Tooltip title="Code Character" placement="right">
             <Button
-              className={classnames('py-2 px-auto commitLog-btn-ctrl', styles.customBtn, {
-                [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.COMMIT_LOG,
+              className={classnames(
+                'py-2 px-auto text-white d-flex justify-content-center',
+                styles.customBtn,
+              )}
+              onClick={() => closeSidePanelTab()}
+            >
+              <FontAwesomeIcon icon={faCode} />
+            </Button>
+          </Tooltip>
+
+          <Tooltip title="Docs" placement="right">
+            <Button
+              className={classnames('py-2 px-auto documentation-btn-ctrl', styles.customBtn, {
+                [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.DOCS,
               })}
-              id="commit_log_button"
+              id="docs_button"
               onClick={() =>
-                sidePanelTab !== SidePanelTab.COMMIT_LOG
-                  ? openSidePanelTab(SidePanelTab.COMMIT_LOG)
+                sidePanelTab !== SidePanelTab.DOCS
+                  ? openSidePanelTab(SidePanelTab.DOCS)
                   : closeSidePanelTab()
               }
             >
-              <FontAwesomeIcon icon={faCodeBranch} />
+              <FontAwesomeIcon icon={faBook} />
             </Button>
-            </BootstrapTooltip>
-          ) : null}
-          {isLoggedIn ? (
-            <BootstrapTooltip title = "Battle TV" placement = "right">
+          </Tooltip>
+          <Tooltip title="Editor Settings" placement="right">
             <Button
-              className={classnames('py-2 px-auto match-btn-ctrl', styles.customBtn, {
-                [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.MATCH,
+              className={classnames('py-2 px-auto editor-settings-btn-ctrl', styles.customBtn, {
+                [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.EDITOR_SETTINGS,
               })}
-              id="matchView_button"
-              onClick={() => {
-                sidePanelTab !== SidePanelTab.MATCH
-                  ? openSidePanelTab(SidePanelTab.MATCH)
-                  : closeSidePanelTab();
-              }}
-            >
-              <FontAwesomeIcon icon={faTv} />
-            </Button>
-            </BootstrapTooltip>
-          ) : null}
-          <BootstrapTooltip title = "Notifications" placement = "right">
-          <Button
-            className={classnames('py-2 px-auto notification-btn-ctrl', styles.customBtn, {
-              [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.NOTIFICATION,
-            })}
-            id="notifications_button"
-            onClick={() =>
-              sidePanelTab !== SidePanelTab.NOTIFICATION
-                ? openSidePanelTab(SidePanelTab.NOTIFICATION)
-                : closeSidePanelTab()
-            }
-          >
-            <FontAwesomeIcon icon={faBell} />
-          </Button>
-          </BootstrapTooltip>
-          <BootstrapTooltip title = {isLoggedIn? "Profile" : "Login" } placement = "right">
-          <Button
-            href={'/#/profile'}
-            className={classnames('py-2 px-auto', styles.customBtn)}
-            id="user_profile_button"
-            onClick={() => {
-              clearAllLogs();
-              if (!isLoggedIn) {
-                setIsAuthenticationOpen(true);
-                return;
+              id="editor_settings_button"
+              onClick={() =>
+                sidePanelTab !== SidePanelTab.EDITOR_SETTINGS
+                  ? openSidePanelTab(SidePanelTab.EDITOR_SETTINGS)
+                  : closeSidePanelTab()
               }
-            }}
-          >
-            <FontAwesomeIcon icon={isLoggedIn ? faUser : faSignInAlt} />
-          </Button>
-          </BootstrapTooltip>
+            >
+              <FontAwesomeIcon icon={faCog} />
+            </Button>
+          </Tooltip>
+          <Tooltip title="Leaderboard" placement="right">
+            <a
+              href="/#/leaderboard"
+              className={classnames('py-2 px-auto leaderboard-btn-ctrl', styles.customBtn, {
+                [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.LEADERBOARD,
+              })}
+              id="leaderboard_button"
+              onClick={() => clearAllLogs()}
+            >
+              <FontAwesomeIcon icon={faTrophy} />
+            </a>
+          </Tooltip>
           {isLoggedIn ? (
-            <BootstrapTooltip title = "Logout" placement = "right">
+            <Tooltip title="Commit Log" placement="right">
+              <Button
+                className={classnames('py-2 px-auto commitLog-btn-ctrl', styles.customBtn, {
+                  [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.COMMIT_LOG,
+                })}
+                id="commit_log_button"
+                onClick={() =>
+                  sidePanelTab !== SidePanelTab.COMMIT_LOG
+                    ? openSidePanelTab(SidePanelTab.COMMIT_LOG)
+                    : closeSidePanelTab()
+                }
+              >
+                <FontAwesomeIcon icon={faCodeBranch} />
+              </Button>
+            </Tooltip>
+          ) : null}
+          {isLoggedIn ? (
+            <Tooltip title="Battle TV" placement="right">
+              <Button
+                className={classnames('py-2 px-auto match-btn-ctrl', styles.customBtn, {
+                  [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.MATCH,
+                })}
+                id="matchView_button"
+                onClick={() => {
+                  sidePanelTab !== SidePanelTab.MATCH
+                    ? openSidePanelTab(SidePanelTab.MATCH)
+                    : closeSidePanelTab();
+                }}
+              >
+                <FontAwesomeIcon icon={faTv} />
+              </Button>
+            </Tooltip>
+          ) : null}
+          <Tooltip title="Notifications" placement="right">
             <Button
+              className={classnames('py-2 px-auto notification-btn-ctrl', styles.customBtn, {
+                [`${styles.customBtnActive}`]: sidePanelTab === SidePanelTab.NOTIFICATION,
+              })}
+              id="notifications_button"
+              onClick={() =>
+                sidePanelTab !== SidePanelTab.NOTIFICATION
+                  ? openSidePanelTab(SidePanelTab.NOTIFICATION)
+                  : closeSidePanelTab()
+              }
+            >
+              <FontAwesomeIcon icon={faBell} />
+            </Button>
+          </Tooltip>
+          <Tooltip title={isLoggedIn ? 'Profile' : 'Login'} placement="right">
+            <Button
+              href={'/#/profile'}
               className={classnames('py-2 px-auto', styles.customBtn)}
-              id="logout_button"
+              id="user_profile_button"
               onClick={() => {
-                logout();
-                this.resetCompleteState();
+                clearAllLogs();
+                if (!isLoggedIn) {
+                  setIsAuthenticationOpen(true);
+                  return;
+                }
               }}
             >
-              <FontAwesomeIcon icon={faSignOutAlt} />
+              <FontAwesomeIcon icon={isLoggedIn ? faUser : faSignInAlt} />
             </Button>
-            </BootstrapTooltip>
+          </Tooltip>
+          {isLoggedIn ? (
+            <Tooltip title="Logout" placement="right">
+              <Button
+                className={classnames('py-2 px-auto', styles.customBtn)}
+                id="logout_button"
+                onClick={() => {
+                  logout();
+                  this.resetCompleteState();
+                }}
+              >
+                <FontAwesomeIcon icon={faSignOutAlt} />
+              </Button>
+            </Tooltip>
           ) : null}
-          <BootstrapTooltip title = "Home" placement = "right">
-          <a
-            className={classnames(
-              'py-2 px-auto notification-btn-ctrl',
-              styles.customBtn,
-              styles.infoCircle,
-            )}
-            href="/#/home"
-            onClick={() => clearAllLogs()}
-          >
-            <FontAwesomeIcon icon={faInfoCircle} />
-          </a>
-          </BootstrapTooltip>
-          <BootstrapTooltip title = "Take a tour" placement = "right">
-          <Button
-            className={classnames('py-2 px-auto', styles.joyRide)}
-            id="joyride_button"
-            onClick={() => this.props.toggleReactTour()}
-          >
-            <FontAwesomeIcon icon={faQuestionCircle} />
-          </Button>
-          </BootstrapTooltip>
-          <BootstrapTooltip title = "Made with ❤ by Delta Force" placement = "right">
-          <Button
-            className={classnames('py-2 px-auto', styles.deltaLogo)}
-            id="delta_logo"
-          >
-            <a href="https://delta.nitt.edu" target="_blank">
-              <img src="assets/img/deltaLogo.png" height={20} width={20} />
+          <Tooltip title="Home" placement="right">
+            <a
+              className={classnames(
+                'py-2 px-auto notification-btn-ctrl',
+                styles.customBtn,
+                styles.infoCircle,
+              )}
+              href="/#/home"
+              onClick={() => clearAllLogs()}
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
             </a>
-          </Button>
-          </BootstrapTooltip>
+          </Tooltip>
+          <Tooltip title="Take a tour" placement="right">
+            <Button
+              className={classnames('py-2 px-auto', styles.joyRide)}
+              id="joyride_button"
+              onClick={() => this.props.toggleReactTour()}
+            >
+              <FontAwesomeIcon icon={faQuestionCircle} />
+            </Button>
+          </Tooltip>
+          <Tooltip title="Made with ❤ by Delta Force" placement="right">
+            <Button className={classnames('py-2 px-auto', styles.deltaLogo)} id="delta_logo">
+              <a href="https://delta.nitt.edu" target="_blank">
+                <img src="assets/img/deltaLogo.png" height={20} width={20} />
+              </a>
+            </Button>
+          </Tooltip>
         </ButtonGroup>
       </div>
     );
