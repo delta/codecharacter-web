0d9ca53f56ccc49d247d77a3edc1a8cd7b061f0b
PRADEEPERIYASAMY <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-03-21 10:04:04 +0530

    feat : Add description for levels

--- src/config/questLevels.ts
+++ src/config/questLevels.ts
@@ -1,22 +1,39 @@
 export const levels = [
   {
-    description: '',
+    description: 'Still sleepy after drinking 2 cups of black coffee, only to enter your own nightmare - a tool ' +
+      'to virtual reality. This is the 2070\'s where once in a dream, you can choose your own adventurous "nightmares".' +
+      ' You have been mentally teleported back in the history of Mayan Civilization! Sadly, now disintegrated due to civil war. ' +
+      'Each kingdom is forced to replace their soldiers with bots, that they can control from the safety of their forts. ' +
+      'You have volunteered to help your kingdom control its army to claim as many treasures as possible. But first, you must train.',
     level: 1,
   },
   {
-    description: '',
+    description: 'Excellent! Completing that task gave you a gush of dopamine release in your brain right!?  ' +
+      'At this point you try to go back to your real world, but you fail to teleport yourself back. ' +
+      'You are wondering why and set to try again- but just then a piercing sonorous siren rings in your kingdom, there has been an attack ' +
+      'laid on your kingdom and you immediately are sent on an assignment to help fight against a local rival group. This is your chance to ' +
+      'stand out among other trainees and be chosen to battle.',
     level: 2,
   },
   {
-    description: '',
+    description: 'Your body is feeling much better and try teleporting back to the real world only to find out that your brain has been hacked. ' +
+      'Hacked by a strange VIRUS. This virus attack has taken over your network of neurons and frozen the network in your brain. ' +
+      'You have no choice but to find the solution. Meanwhile you noticed that the bot has learned how to move to the correct place, ' +
+      'but you cannot win the battle just by that. You must destroy the enemy also, and for that you need to train your bot to blast at ' +
+      'the right position.',
     level: 3,
   },
   {
-    description: '',
+    description: 'After completion you realize that you are slowly regaining consciousness in the real world. Completing each level successfully ' +
+      'releases fiery hormones in your body which fires up your neurons which helps regain some part of your consciousness to the real world. ' +
+      'In every battle, one saves his best troops for the perfect time, which can turn the result of the battle for them any time. ' +
+      'The enemy has increased their power too. You have been granted time to become stronger. To learn and improvise against the wrong doers.',
     level: 4,
   },
   {
-    description: '',
+    description: 'You have arrived at the final stage. Once done you will be let loose of this mental state of captive. This task is where ' +
+      'you must do some unlearning and relearning. Analyse opponents’ tactics and carefully apply the skills that you have learned up until now' +
+      ' to finally defeat the brat once and for all!',
     level: 5,
   },
 ];
