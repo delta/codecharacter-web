b1ef3b4271cc1f7a092c74541c9d5e884c0b57b9
Rinish Sam <36656347+CaptainIRS@users.noreply.github.com>
2021-03-31 00:30:46 +0530

    Fix UI bugs and server config (#83)
    * Fix preloader error
    
    * Readd server config
    
    * Minor ui fixes in many components
    
    * Fix z-index of renderer panel separator
    
    * fix(leaderboard): fix text overlapping
    
    Co-authored-by: Bestin <bestinbthomas@gmail.com>

--- src/app/components/UserProfileModal/EditPassword.tsx
+++ src/app/components/UserProfileModal/EditPassword.tsx
@@ -12,109 +12,105 @@ export class EditPassword extends React.Component<EditPasswordInterfaces.Props, 
     const { editPasswordRef } = this.props;
     const { oldPassword, password, repeatPassword } = this.props;
     return (
-      <div className="col-5">
-        <Row className={classnames('mb-3')}>
-          <div className={classnames('col-sm-12', styles.passwordForm)}>
-            <div
-              className={classnames('text-dark', styles.formHeading)}
-              style={{
-                marginBottom: '5%',
-                marginLeft: '-1px',
-                textAlign: 'center',
-              }}
-            >
-              User Credentials
-            </div>
-            <form
-              className={'editpasswordForm'}
-              noValidate
-              ref={editPasswordRef}
-              onSubmit={handleEditPassword}
-            >
-              <div className="form-row">
-                <div className="col mb-3">
-                  <div className={classnames(styles1['input-group'])}>
-                    <label className="labeltext">Old Password</label>
-                    <div className={classnames('input-group-prepend', styles.inputGroup)}>
-                      <input
-                        type="password"
-                        className={classnames('form-control', styles1['login-input'])}
-                        id="editValidationOldPassword"
-                        aria-describedby="inputGroupPrepend"
-                        minLength={5}
-                        value={oldPassword}
-                        onChange={(e) => onInputChange(InputName.oldPassword, e.target.value)}
-                        required
-                      />
-                      <div className={classnames('invalid-feedback', styles1['login-error'])}>
-                        Password should have minimum 5 characters.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="form-row">
-                <div className="col mb-3">
-                  <div className={classnames(styles1['input-group'])}>
-                    <label className="labeltext">New Password</label>
-                    <div className={classnames('input-group-prepend', styles.inputGroup)}>
-                      <input
-                        type="password"
-                        className={classnames('form-control', styles1['login-input'])}
-                        id="editValidationPassword"
-                        aria-describedby="inputGroupPrepend"
-                        minLength={5}
-                        value={password}
-                        onChange={(e) => onInputChange(InputName.password, e.target.value)}
-                        required
-                      />
-                      <div className={classnames('invalid-feedback', styles1['login-error'])}>
-                        Password should have minimum 5 characters.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="form-row">
-                <div className="col mb-3">
-                  <div className={classnames(styles1['input-group'])}>
-                    <label className="labeltext">Confirm Password</label>
-                    <div className={classnames('input-group-prepend', styles.passwordInput)}>
-                      <input
-                        type="password"
-                        className={classnames('form-control', styles1['login-input'])}
-                        id="editValidationRepeatPassword"
-                        aria-describedby="inputGroupPrepend"
-                        minLength={5}
-                        value={repeatPassword}
-                        onChange={(e) => onInputChange(InputName.repeatPassword, e.target.value)}
-                        required
-                      />
-                      <div className={classnames('invalid-feedback', styles1['login-error'])}>
-                        Passwords should match.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="form-row">
-                <div className="col-12 text-center">
-                  <button
-                    className={classnames(
-                      'btn btn-success',
-                      styles1.loginButton,
-                      styles.loginButton,
-                    )}
-                    type="submit"
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
-            </form>
+      <Row className={classnames('mb-3')}>
+        <div className="col-sm-2 col-md-3 col-lg-4"></div>
+        <div className={classnames('col-sm-8 col-md-6 col-lg-4', styles.passwordForm)}>
+          <div
+            className={classnames('text-dark', styles.formHeading)}
+            style={{
+              marginBottom: '5%',
+              marginLeft: '-1px',
+              textAlign: 'center',
+            }}
+          >
+            User Credentials
           </div>
-        </Row>
-      </div>
+          <form
+            className={'editpasswordForm'}
+            noValidate
+            ref={editPasswordRef}
+            onSubmit={handleEditPassword}
+          >
+            <div className="form-row">
+              <div className="col mb-3">
+                <div className={classnames(styles1['input-group'])}>
+                  <label className="labeltext">Old Password</label>
+                  <div className={classnames('input-group-prepend', styles.inputGroup)}>
+                    <input
+                      type="password"
+                      className={classnames('form-control', styles1['login-input'])}
+                      id="editValidationOldPassword"
+                      aria-describedby="inputGroupPrepend"
+                      minLength={5}
+                      value={oldPassword}
+                      onChange={(e) => onInputChange(InputName.oldPassword, e.target.value)}
+                      required
+                    />
+                    <div className={classnames('invalid-feedback', styles1['login-error'])}>
+                      Password should have minimum 5 characters.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="col mb-3">
+                <div className={classnames(styles1['input-group'])}>
+                  <label className="labeltext">New Password</label>
+                  <div className={classnames('input-group-prepend', styles.inputGroup)}>
+                    <input
+                      type="password"
+                      className={classnames('form-control', styles1['login-input'])}
+                      id="editValidationPassword"
+                      aria-describedby="inputGroupPrepend"
+                      minLength={5}
+                      value={password}
+                      onChange={(e) => onInputChange(InputName.password, e.target.value)}
+                      required
+                    />
+                    <div className={classnames('invalid-feedback', styles1['login-error'])}>
+                      Password should have minimum 5 characters.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="col mb-3">
+                <div className={classnames(styles1['input-group'])}>
+                  <label className="labeltext">Confirm Password</label>
+                  <div className={classnames('input-group-prepend', styles.passwordInput)}>
+                    <input
+                      type="password"
+                      className={classnames('form-control', styles1['login-input'])}
+                      id="editValidationRepeatPassword"
+                      aria-describedby="inputGroupPrepend"
+                      minLength={5}
+                      value={repeatPassword}
+                      onChange={(e) => onInputChange(InputName.repeatPassword, e.target.value)}
+                      required
+                    />
+                    <div className={classnames('invalid-feedback', styles1['login-error'])}>
+                      Passwords should match.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="col-12 text-center">
+                <button
+                  className={classnames('btn btn-success', styles1.loginButton, styles.loginButton)}
+                  type="submit"
+                >
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="col-sm-2 col-md-3 col-lg-4"></div>
+      </Row>
     );
   }
 }
--- src/app/components/UserProfileModal/index.tsx
+++ src/app/components/UserProfileModal/index.tsx
@@ -53,51 +53,47 @@ export class UserProfileModal extends React.Component<
     return (
       <Grid fluid={true} className={classnames(styles.UserEdit)} style={{ padding: '0' }}>
         <NavBar isLoggedIn={true} page={NavPage.PROFILE} />
-        <Row
-          className={
-            this.state.isPasswordPage
-              ? classnames(styles.editProfileElement)
-              : classnames(styles.editPasswordElement)
-          }
-        >
-          {this.state.isPasswordPage ? (
-            <EditProfile
-              handleEditProfile={this.handleEditProfile}
-              onInputChange={this.onInputChange}
-              editProfileRef={this.editProfileRef}
-              username={username}
-              fullName={fullName}
-              userDetails={userDetails}
-              country={country}
-              avatar={avatar}
-            />
-          ) : (
-            <EditPassword
-              handleEditPassword={this.handleEditPassword}
-              onInputChange={this.onInputChange}
-              editPasswordRef={this.editPasswordRef}
-              oldPassword={oldPassword}
-              password={password}
-              repeatPassword={repeatPassword}
-              userDetails={userDetails}
-            />
-          )}
-        </Row>
+        {this.state.isPasswordPage ? (
+          <EditProfile
+            handleEditProfile={this.handleEditProfile}
+            onInputChange={this.onInputChange}
+            editProfileRef={this.editProfileRef}
+            username={username}
+            fullName={fullName}
+            userDetails={userDetails}
+            country={country}
+            avatar={avatar}
+          />
+        ) : (
+          <EditPassword
+            handleEditPassword={this.handleEditPassword}
+            onInputChange={this.onInputChange}
+            editPasswordRef={this.editPasswordRef}
+            oldPassword={oldPassword}
+            password={password}
+            repeatPassword={repeatPassword}
+            userDetails={userDetails}
+          />
+        )}
         <Row>
-          <a
-            className={
-              this.state.isPasswordPage
-                ? classnames('labeltext', styles.loginPageLink)
-                : classnames('labeltext', styles.passwordPageLink)
-            }
-            onClick={() => {
-              this.setState((prevState) => ({
-                isPasswordPage: !prevState.isPasswordPage,
-              }));
-            }}
-          >
-            {this.state.isPasswordPage ? 'Want to change Credentials?' : 'Want to change Info?'}
-          </a>
+          <div className="col-sm-2 col-md-3 col-lg-4"></div>
+          <div className="col-sm-8 col-md-6 col-lg-4 text-center">
+            <a
+              className={
+                this.state.isPasswordPage
+                  ? classnames('labeltext', styles.loginPageLink)
+                  : classnames('labeltext', styles.passwordPageLink)
+              }
+              onClick={() => {
+                this.setState((prevState) => ({
+                  isPasswordPage: !prevState.isPasswordPage,
+                }));
+              }}
+            >
+              {this.state.isPasswordPage ? 'Want to change Credentials?' : 'Want to change Info?'}
+            </a>
+          </div>
+          <div className="col-sm-2 col-md-3 col-lg-4"></div>
         </Row>
         {this.state.isPasswordPage ? (
           <Row>
