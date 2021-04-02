7756334ac1ba99d40d102a7da8335dfea5a74706
Rinish Sam <36656347+CaptainIRS@users.noreply.github.com>
2021-04-02 18:04:18 +0530

    Cache build output appropriately (#91)

--- server/app.js
+++ server/app.js
@@ -2,7 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build'), {
+  etag: true,
+  lastModified: true,
+  setHeaders: (res, path) => {
+    if (path.endsWith('.html') || path.endsWith('bundle.js')) {
+      res.setHeader('Cache-Control', 'no-cache');
+    } else {
+      const cacheMaxAge = 60 * 60 * 24 * 100; // 100 days
+      res.setHeader('Cache-Control', `max-age=${cacheMaxAge}`);
+    }
+  },
+}));
 
 app.use(function(req, res, next) {
   if (req.path.length > 1 && /\/$/.test(req.path)) {
